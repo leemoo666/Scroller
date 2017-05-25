a12554d9f37a53d019b4f65ced5414020865b35f
ll201lw@163.com <lixiaoming@ycx.net>
2017-05-25 15:32:33 +0800

    init

---
+++ Scroller/app/src/main/java/com/wyc/scroller/ScrollerView.java
@@ -0,0 +1,143 @@
+package com.wyc.scroller;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+/**
+ * Created by 李小明 on 17/5/13.
+ * 邮箱:287907160@qq.com
+ */
+
+public class ScrollerView extends ViewGroup {
+
+    /**
+     * 用于完成滚动操作的实例
+     */
+    private Scroller mScroller;
+
+    /**
+     * 判定为拖动的最小移动像素数
+     */
+    private int mTouchSlop;
+
+    /**
+     * 手机按下时的屏幕坐标
+     */
+    private float mXDown;
+
+    /**
+     * 手机当时所处的屏幕坐标
+     */
+    private float mXMove;
+
+    /**
+     * 上次触发ACTION_MOVE事件时的屏幕坐标
+     */
+    private float mXLastMove;
+
+    /**
+     * 界面可滚动的左边界
+     */
+    private int leftBorder;
+
+    /**
+     * 界面可滚动的右边界
+     */
+    private int rightBorder;
+
+
+    /**
+     * 可滑出的最大界限边界
+     */
+    private int outBorder = 300;
+
+    public ScrollerView(Context context) {
+        super(context);
+    }
+
+    public ScrollerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public ScrollerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mScroller = new Scroller(context);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (changed) {
+            int childCount = getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View childView = getChildAt(i);
+                // 为ScrollerLayout中的每一个子控件在水平方向上进行布局
+                childView.layout(i * childView.getMeasuredWidth(), 0, (i + 1) * childView.getMeasuredWidth(), childView.getMeasuredHeight());
+            }
+            // 初始化左右边界值
+            leftBorder = getChildAt(0).getLeft();
+            rightBorder = getChildAt(getChildCount() - 1).getRight();
+
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mXDown = event.getRawX();
+                mXLastMove = mXDown;
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                mXMove = event.getRawX();
+                int scrolledX = (int) (mXLastMove - mXMove);
+                if (getScrollX() + scrolledX < leftBorder - outBorder) {
+                    scrollTo(leftBorder - outBorder, 0);
+                    return true;
+                } else if (getScrollX() + getWidth() + scrolledX > rightBorder + outBorder) {
+                    scrollTo(rightBorder - getWidth() + outBorder, 0);
+                    return true;
+                }
+                scrollBy(scrolledX, 0);
+                mXLastMove = mXMove;
+                break;
+            case MotionEvent.ACTION_UP:
+                if (getScrollX() < (rightBorder - getWidth()) / 3) {
+                    mScroller.startScroll(getScrollX(), 0, -getScrollX(), 0);
+                } else if (getScrollX() > (2 * (rightBorder - getWidth()) / 3)) {
+                    mScroller.startScroll(getScrollX(), 0, rightBorder - getWidth() - getScrollX(), 0);
+                }
+                invalidate();
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void computeScroll() {
+        // 第三步，重写computeScroll()方法，并在其内部完成平滑滚动的逻辑
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            invalidate();
+        }
+    }
+}
